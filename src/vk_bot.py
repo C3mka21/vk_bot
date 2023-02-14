df899031d5b5ff8cbb71b0e5a77a74fd4d3e170b
AeeLov3 <87200908+C3mka21@users.noreply.github.com>
2023-02-14 12:10:42 +0300

    Бот готов к использованию

--- src/vk_bot.py
+++ src/vk_bot.py
@@ -30,7 +30,7 @@ class VkBot:
         self._USER_ID = user_id
         self._USERNAME = self._get_user_name_from_vk_id(user_id)
 
-        self._COMMANDS = ["НАЧАТЬ","ИГРА","ПУТЬ"]
+        self._COMMANDS = ["НАЧАТЬ","ИГРА","ПУТЬ","ПОМОЩЬ","РЕСТАРТ"]
 
     def _get_user_name_from_vk_id(self, user_id):
         request = requests.get("https://vk.com/id" + str(user_id))
@@ -40,17 +40,33 @@ class VkBot:
         return user_name.split()[0]
 
     def new_message(self, message):
+        can_i_go = False
         if message.upper() == self._COMMANDS[0]:
-
             return f"Привет-привет, {self._USERNAME}!\nДобро пажаловать в мою игру! Введи 'игра' если хочеть начать игру"
-
+        elif message.upper() == self._COMMANDS[4]:
+            change_pos(1)
+            return f"Добро пожаловать, {self._USERNAME}!\nВы находитесь на центральной площади\nВведите 'путь' чтобы узнать куда вы можете пройти"
+        elif message.upper() == self._COMMANDS[3]:
+           return ('\n'.join(self._COMMANDS))
+        elif str.isnumeric(message.upper()):
+            peak_to_go = int(message.upper())
+            if peak_to_go <= len(data):
+                splitted = data[get_pos()][0]["peaks_i_can_reach"].split()
+                for i in range(len(splitted)):
+                    if peak_to_go == int(splitted[i]):
+                        can_i_go = True
+                if can_i_go:
+                    change_pos(peak_to_go)
+                    return data[get_pos()][1]["infoaboutplace"]
+                else:
+                    return "Вы не можете попасть на данный этап"
+            else:
+                return "Вы не можете попасть на данный этап"
         elif message.upper() == self._COMMANDS[1]:
-            print('Добро пожаловать!')
-            print("Вы находитесь на центральной площади")
-
-            return f"Добро пожаловать, {self._USERNAME}!\nВы находитесь на центральной площади"
+            change_pos(1)
+            return f"Добро пожаловать, {self._USERNAME}!\nВы находитесь на центральной площади\nВведите 'путь' чтобы узнать куда вы можете пройти"
         elif message.upper() == self._COMMANDS[2]:
             return f'Сейчас вы можете:\n{data[get_pos()][0]["info"]} \nвведите {data[get_pos()][0]["peaks_i_can_reach"]}'
 
         else:
-            return "Не понимаю о чем вы..."
+            return "Не понимаю о чем вы...\nВведите 'помощь' чтобы узнать что умеет бот"
